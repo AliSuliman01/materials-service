589f3cb0757419dfb15d7dcd42c7bff8dea0e8e0
Ali Suliman <alisulimanq@gmail.com>
2022-09-20 06:01:58 +0300

    - prefix routes.

--- routes/Materials/Courses/Courses/courses.php
+++ routes/Materials/Courses/Courses/courses.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Materials\Courses\Courses\CoursesController;
 
+Route::prefix('courses')->group(function(){
+
 Route::get('search',[CoursesController::class, 'dataForSearch']);
 Route::post('search',[CoursesController::class, 'search']);
 
+});
 Route::apiResource('courses',CoursesController::class);
 
