8200fa46be2caea6fd5693de2b8f762aa671fe00
Ali Suliman <alisulimanq@gmail.com>
2022-09-20 08:23:54 +0300

    fix

--- app/Domain/Materials/Materials/DTO/MaterialDTO.php
+++ app/Domain/Materials/Materials/DTO/MaterialDTO.php
@@ -9,21 +9,23 @@ use Spatie\DataTransferObject\DataTransferObject;
 class MaterialDTO extends DataTransferObject
 {
 
-	/* @var integer|null */
-	public $id;
-	/* @var integer|null */
-	public $duration;
-	/* @var integer|null */
-	public $level_id;
+    /* @var integer|null */
+    public $id;
+    /* @var integer|null */
+    public $duration;
+    /* @var integer|null */
+    public $level_id;
+    /* @var integer|null */
+    public $is_featured;
 
 
     public static function fromRequest($request)
     {
         return new self([
-			'id'				=> $request['id'] ?? null ,
-			'duration'				=> $request['duration'] ?? null ,
-			'level_id'				=> $request['level_id'] ?? null ,
-
+            'id' => $request['id'] ?? null,
+            'duration' => $request['duration'] ?? null,
+            'level_id' => $request['level_id'] ?? null,
+            'is_featured' => $request['is_featured'] ?? null,
         ]);
     }
 }
--- app/Http/Requests/Materials/Specialisations/Specialisations/StoreSpecialisationRequest.php
+++ app/Http/Requests/Materials/Specialisations/Specialisations/StoreSpecialisationRequest.php
@@ -13,6 +13,7 @@ class StoreSpecialisationRequest extends FormRequest
         return [
             'duration'				=> 'integer|required' ,
             'level_id'				=> 'integer|required' ,
+            'is_featured'				=> 'integer|nullable' ,
             'translations' => 'required|array',
             'translations.*.language_code' => 'required',
             'translations.*.name' => 'required|string',
